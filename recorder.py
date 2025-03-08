d4f5274fdd25c5abeabcd502d8e68d8a60132dc4
Thinking-Robot-Co <mythinkingrobot@gmail.com>
2025-03-08 20:17:54 +0530

    Modified zoom FPS and all

--- recorder.py
+++ recorder.py
@@ -161,7 +161,7 @@ class VideoRecorder:
             # Non-audio mode: use the standard segmentation monitoring.
             self.video_start_time = datetime.datetime.now()
             self.current_video_file = self.generate_video_filename()
-            self.camera.apply_video_transform(hflip=True, vflip=False, rotation=90, width=1280, height=720)
+            # self.camera.apply_video_transform(hflip=True, vflip=False, rotation=90, width=1280, height=720)
             self.camera.picam2.start_and_record_video(self.current_video_file)
             self.segments.append(self.current_video_file)
             self.stop_monitor = False
