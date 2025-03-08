62fdbc352ea9f390e5aff4e43456fbe55e27d38f
Thinking-Robot-Co <mythinkingrobot@gmail.com>
2025-03-08 22:49:21 +0530

    Recorder.py is it

--- recorder.py
+++ recorder.py
@@ -163,7 +163,9 @@ class VideoRecorder:
             # Non-audio mode: use the standard segmentation monitoring.
             self.video_start_time = datetime.datetime.now()
             self.current_video_file = self.generate_video_filename()
-            self.camera.apply_video_transform(hflip=False, vflip=False, rotation=0)
+            config = self.camera.picam2.create_video_configuration()
+            self.camera.picam2.configure(config)
+            # self.camera.apply_video_transform(hflip=False, vflip=False, rotation=0)
             self.camera.picam2.start_and_record_video(self.current_video_file)
             self.segments.append(self.current_video_file)
             self.stop_monitor = False
